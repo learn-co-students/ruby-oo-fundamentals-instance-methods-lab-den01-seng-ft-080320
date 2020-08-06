27ac310250ebb42bfbab191302484ee5952c4955
Kyle Trahan <ktrahan2@gmail.com>
2020-08-06 11:01:35 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is Sitting"
+    end
+
+    
+end
---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person
+
+    def talk
+        puts "Hellow World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+
+end
+
